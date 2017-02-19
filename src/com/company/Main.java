5354d267a29e5bc620c846b5c99e4107f4c8c191
ZloiY <NoRFolf1000@mail.ru>
2017-02-19 22:58:59 +0300

    add sigint and socket list

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,31 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+    private static List<Socket> socketList;
+    private static ServerSocket serverSocket;
     public static void main(String[] args) {
+        socketList = new ArrayList<Socket>();
         try{
-            ServerSocket serverSocket = new ServerSocket(1488);
+            Runtime.getRuntime().addShutdownHook(new Thread(){
+                @Override
+                public void run() {
+                    System.out.println("SIGINT Shutting down");
+                    try {
+                        System.out.println();
+                        if (!socketList.isEmpty())
+                            for (Socket socket : socketList)
+                                socket.close();
+                        serverSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+             serverSocket = new ServerSocket(1488);
             System.out.println("Server start..");
             acceptClients(serverSocket);
         }catch(IOException e){
@@ -22,6 +42,7 @@ public class Main {
     private static void acceptClients(ServerSocket serverSocket) throws IOException{
         while (true){
             Socket socket = serverSocket.accept();
+            socketList.add(socket);
             ConnectionThread connectionThread = new ConnectionThread(socket);
             connectionThread.start();
         }
